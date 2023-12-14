56b5c2be9122f2e9f2477a0f13c29d95e1111e7b
fmruiz <x.francoruiz@gmail.com>
2023-12-13 22:03:04 -0300

    wip sidebar

--- components/NavLinks.jsx
+++ components/NavLinks.jsx
@@ -1,7 +1,25 @@
+import Link from 'next/link';
 import React from 'react';
 
+const links = [
+    { href: '/chat', label: 'chat' },
+    { href: '/tours', label: 'tours' },
+    { href: '/tours/new-tour', label: 'new tour' },
+    { href: '/profile', label: 'profile' },
+];
+
 const NavLinks = () => {
-    return <div>NavLinks</div>;
+    return (
+        <div className="menu text-base-content">
+            {links.map((link) => {
+                return (
+                    <li key={link.href}>
+                        <Link href={link.href}>{link.label}</Link>
+                    </li>
+                );
+            })}
+        </div>
+    );
 };
 
 export default NavLinks;
